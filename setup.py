be14daf32fa8bb8760e654f0355a14bb957edeb2
Chris McKenzie <kristopolous@yahoo.com>
2020-04-21 01:13:06 -0700

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mansnip-kristopolous", 
-    version="0.2",
+    version="0.3",
     scripts=["mansnip"],
     author="Chris McKenzie",
     author_email="kristopolous@yahoo.com",
