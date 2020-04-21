34f3b2d82b743395b4d748ff6195c24be0a00d3e
Chris McKenzie <kristopolous@yahoo.com>
2020-04-20 21:20:14 -0700

    satisfying pypi

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mansnip-kristopolous", 
+    version="0.1",
+    author="Chris McKenzie",
+    author_email="kristopolous@yahoo.com",
+    description="Finding just the snippets in man page you care about",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kristopolous/mansnip",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.2',
+)
