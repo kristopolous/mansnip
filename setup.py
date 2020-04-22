c729cafae13c16e343e243aef1df36fe59dc4574
Chris McKenzie <kristopolous@yahoo.com>
2020-04-22 02:35:44 -0700

    bugfixes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mansnip-kristopolous", 
-    version="0.3.2",
+    version="0.3.3",
     scripts=["mansnip"],
     author="Chris McKenzie",
     author_email="kristopolous@yahoo.com",
