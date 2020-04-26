8e736e27da47896aa381957ea55bcf63dfa02bd3
Chris McKenzie <kristopolous@yahoo.com>
2020-04-25 22:14:06 -0700

    version bump for pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mansnip-kristopolous", 
-    version="0.3.3",
+    version="0.3.4",
     scripts=["mansnip"],
     author="Chris McKenzie",
     author_email="kristopolous@yahoo.com",
